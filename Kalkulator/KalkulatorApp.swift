2a9cb2667d4f4f916cb3a7313536a1dbf620a4c4
Damian_Mydlak <mydlak51@gmail.com>
2023-10-09 05:47:31 -0700

    Initial Commit

---
+++ Kalkulator/KalkulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalkulatorApp.swift
+//  Kalkulator
+//
+//  Created by Damian on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct KalkulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
